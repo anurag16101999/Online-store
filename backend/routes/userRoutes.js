f3cf56fc624db6cadfebe10a64501b83e1fe1763
Anurag Gupta <guptaanurag1610@gmail.com>
2021-01-19 19:04:05 +0530

    Added user and password auth

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+const router = express.Router();
+import { authUser } from "../controllers/userController.js";
+
+router.post("/login", authUser);
+//router.route("/:id").get(getProductById);
+
+export default router;
