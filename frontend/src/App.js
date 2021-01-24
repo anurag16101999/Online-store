18b65ef86731197953d4c24117a336588d263b25
Anurag Gupta <guptaanurag1610@gmail.com>
2021-01-24 16:17:32 +0530

    Added order list screen for admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
-
+import OrderListScreen from "./screens/OrderListScreen";
 const App = () => {
   return (
     <Router>
@@ -36,6 +36,7 @@ const App = () => {
           <Route path="/admin/userList" component={UserListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/admin/productList" component={ProductListScreen} />
+          <Route path="/admin/orderList" component={OrderListScreen} />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/" component={HomeScreen} exact />
         </Container>
